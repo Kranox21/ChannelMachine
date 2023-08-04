3327ff8da53d0197b842c666a86d5df6ea88df49
Ashton Jensen <ashton.jensen21@gmail.com>
2023-08-03 17:40:57 -0700

    Channel Machine: Closed Alpha 0.3a
    - Many Bug fixes and stability improvements
    - Code cleanup and variable renaming
    - Python script update attempt to support cross-platform from other OS (Needs Testing)
    - Organization improvements such as texture naming conventions for batch process and texture export process.
    -

--- Scripts/OpenWindowsDirectory.py
+++ Scripts/OpenWindowsDirectory.py
@@ -1,11 +1,15 @@
 import tkinter
 from tkinter import filedialog
+import os
+import platform
 
 root = tkinter.Tk()
 root.withdraw()
+system = platform.system()
 
-folder_path = filedialog.askdirectory()
+folder_path = filedialog.askdirectory() + '/'
 
-folder_path + "/"
+if (system("Windows")):
+    folder_path = folder_path.replace('/', '\\')  
 
 print(folder_path)
